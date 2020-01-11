fc4a6dddef12389ebc4545aca7b333c47e43e748
loldabigboi <loldabigboi@gmail.com>
2020-01-12 10:58:11 +1300

    Reworked NeuralNetwork class to use the FullyConnectedLayer class

--- neuralNetwork.js
+++ neuralNetwork.js
@@ -5,69 +5,10 @@ class NeuralNetwork {
      * @param {number[]} neuronCountArray An array of numbers corresponding to the number of
      * neurons in each layer (first entry is input layer, last entry is output layer).
      */
-     constructor(neuronCountArray, options) {
+     constructor(options) {
 
-        this.neurons = [];
-        for (let i = 0; i < neuronCountArray.length; i++) {
-             this.neurons.push(Array.from(Array(neuronCountArray[i]), () => 0));
-        }
-
-        this.minBias = (options.minBias != undefined) ? options.minBias : -1;
-        this.maxBias = (options.maxBias != undefined) ? options.maxBias : 1;
-
-        // although biases are present for the inputs, this is superficial. it has no bearing
-        // on any of the calculations, and was simply done to make indexing simpler.
-        this.biases = this.neurons.map((val) => val.map((_val) => this.minBias + Math.random()*(this.maxBias - this.minBias)));
-
-        this.minWeight = (options.minWeight != undefined) ? options.minWeight : -1;
-        this.maxWeight = (options.maxWeight != undefined) ? options.maxWeight : 1;
-
-        this.weights = [];
-        for (let i = 0; i < this.neurons.length-1; i++) {
-            let numCols = this.neurons[i].length,
-                numRows = this.neurons[i+1].length;
-            this.weights[i] = new Matrix(numRows, numCols);
-            this.weights[i].map(() => this.minWeight + Math.random()*(this.maxWeight - this.minWeight));  // randomly initialise all weights
-        }
-
-        this.hiddenActivationType = (options.hiddenActivationType) ? options.hiddenActivationType : 
-                                                                     NeuralNetwork.SIGMOID;
-        if (this.hiddenActivationType == NeuralNetwork.SIGMOID) {
-            this.hiddenActivationFn        = sigmoid;
-            this.dHiddenActivationFn       = dSigmoid;
-            this.hiddenGDFn                = oneToOneActivationGD;
-        } else if (this.hiddenActivationType == NeuralNetwork.TANH) {
-            this.hiddenActivationFn        = tanh;
-            this.dHiddenActivationFn       = dTanh;
-            this.hiddenGDFn                = oneToOneActivationGD;
-        } else if (this.hiddenActivationType == NeuralNetwork.RELU) {
-            this.hiddenActivationFn        = relu;
-            this.dHiddenActivationFn       = dRelu;
-            this.hiddenGDFn                = oneToOneActivationGD;
-        } else {
-            throw new Error("Invalid hidden activation function specified (note that softmax cannot be used for hidden layers).");
-        }
-
-        this.outputActivationType = (options.outputActivationType) ? options.outputActivationType : 
-                                                                     NeuralNetwork.SIGMOID;
-        if (this.outputActivationType == NeuralNetwork.SIGMOID) {
-            this.outputActivationFn        = sigmoid;
-            this.dOutputActivationFn       = dSigmoid;
-            this.outputGDFn                = oneToOneActivationGD;
-        } else if (this.outputActivationType == NeuralNetwork.TANH) {
-            this.outputActivationFn        = tanh;
-            this.dOutputActivationFn       = dTanh;
-            this.outputGDFn                = oneToOneActivationGD;
-        } else if (this.outputActivationType == NeuralNetwork.RELU){
-            this.outputActivationFn        = relu;
-            this.dOutputActivationFn       = dRelu;
-            this.outputGDFn                = oneToOneActivationGD;
-        } else if (this.outputActivationType == NeuralNetwork.SOFTMAX) {
-            this.outputActivationFn        = softmax;
-            this.outputGDFn                = crossEntropySoftmaxGD;
-        } else {
-            throw new Error("Invalid output activation function specified");
-        }
+        this.layerBlueprints = [];
+        this.layers = [];
 
         this.errorType = (options.errorType) ? options.errorType : 
                                                NeuralNetwork.SQUARED_ERROR;
@@ -81,111 +22,106 @@ class NeuralNetwork {
 
         this.learningRate = (options.learningRate) ? options.learningRate : 0.1;
 
-    }  
+    }
 
-    /**
-     * Performs an iteration of the feed foward algorithm, from the layer whose index is specified by
-     * "from" to the next layer (if none exists then the function simply returns - this is the base case).
-     * @param {number[] | Matrix} values 
-     * @param {number} from 
-     */
-    _feedForward(values, from) {
+    addLayer(type, options) {
+        this.layerBlueprints.push({
+            type,
+            options
+        });
+    }
 
-        if (from === 0) {  // "from" is 0
-            if (values.length != this.neurons[0].length) {
-                throw new Error("Number of inputs does not match number of input neurons");
+    compile() {
+
+        if (!this.layerBlueprints) {
+            throw new Error("No layers to compile!");
+        }
+
+        this.layers = [];
+
+        for (let i = 0; i < this.layerBlueprints.length; i++) {
+
+            let layerType = this.layerBlueprints[i].type;
+            let nextLayerType = this.layerBlueprints[i+1].type;
+            let layerOptions = this.layerBlueprints[i].options;
+            let nextLayerOptions = this.layerBlueprints[i].options;
+
+            if (i === 0 && !layerOptions.numInputs) {  // first hidden layer
+                throw new Error("First hidden error must specify number of inputs!");
             }
-            this.neurons[0] = values.slice();  // copy array
-        } else if (!(typeof from === "number")) {
-            throw new Error("From must be passed, and must be a number.");
-        }
 
-        let weightsMatrix = this.weights[from];
-        if (!weightsMatrix) {  // base case
-            return;
-        }
-
-        let prevActivations;
-        if (values instanceof Matrix) {
-
-            if (weightsMatrix.cols != values.rows) {
-                throw new Error(`Matrix ${values.rows + '*' + values.cols} 
-                                 not compatible with matrix ${weightsMatrix.rows + '*' + weightsMatrix.cols}.`);
+            if (layerType === Layer.FULLY_CONNECTED) {
+                this.layers.push(new FullyConnectedLayer(layerOptions.numNeurons,
+                                                            nextLayerOptions.numNeurons ));
             } else {
-                prevActivations = values;
+                throw new Error("Only supported layer type atm is fully-connected layers.");
             }
 
-        } else if (values.length !== weightsMatrix.cols) {
-            throw new Error(`Values only has ${values.length} elements but needs ${weightsMatrix.cols}.`);
-        } else {
-            prevActivations = Matrix.fromArray(values, Matrix.COLUMN);
         }
-        
-        let activationFn = (from === this.neurons.length-2) ?
-                            this.outputActivationFn :
-                            this.hiddenActivationFn;
-        let newActivations = activationFn(Matrix.product(weightsMatrix, prevActivations));
-
-        for (let i = 0; i < this.neurons[from+1].length; i++) {
-            this.neurons[from+1][i] = newActivations.get(i, 0);
-        }
-
-        // recursive call
-        this._feedForward(newActivations, from+1);
 
     }
 
     /**
-     * Performs one step of the back-propagation algorithm.
-     * @param {*} layerIndex The index of the layer we are currently propagating from
+     * Feeds the passed inputs forwards through the neural network.
+     * @param {number[] | Matrix} inputs 
      */
-    _backPropagation(layerIndex, _dErrorActivationMatrix, targets) {
+    _feedForward(inputs) {
 
-        if (layerIndex < 1) {  // we have reached input layer
-            return;
-        }
-        
-        let neurons            = this.neurons[layerIndex],
-            prevNeurons        = this.neurons[layerIndex-1];
+        let prevActivations = (inputs instanceof Matrix) ? new Matrix(inputs) : 
+                                                           Matrix.fromArray(inputs, Matrix.COLUMN);
+        for (let i = 0; i < this.layers.length-1; i++) {
 
-        let biases = this.biases[layerIndex];
+            let currLayer = this.layers[i];
 
-        let weights = this.weights[layerIndex-1],  // there is one more layer than weight matrices
-            weightsT = Matrix.transpose(weights);
-
-        let dErrorActivationMatrix;
-        if (!_dErrorActivationMatrix) {  // we must be at the output layer, otherwise this would be passed to us from prev call
-            
-            if (!targets) { // we need target values to calculate error derivative
-                throw new Error("Targets must be provided for output layer.");
+            if (i === 0 && values.length != currLayer.numInputs) {
+                throw new Error("Number of inputs does not match number of input neurons");
             }
-            dErrorActivationMatrix = this.dErrorFn(Matrix.fromArray(neurons, Matrix.COLUMN), 
-                                                   Matrix.fromArray(targets, Matrix.COLUMN));
-        } else {
-            dErrorActivationMatrix = new Matrix(_dErrorActivationMatrix);
+
+            currLayer.neurons = Matrix.product(currLayer.weights, prevActivations);
+            prevActivations = currLayer.neurons;
+
         }
 
-        let {
-            dErrorWeightMatrix,
-            dErrorBiasMatrix,
-            dErrorPrevActivationMatrix
-        } = (_dErrorActivationMatrix) ? this.hiddenGDFn(neurons, prevNeurons, biases, weights, 
-                                                        targets, dErrorActivationMatrix, this.dHiddenActivationFn) :
-                                        this.outputGDFn(neurons, prevNeurons, biases, weights, 
-                                                        targets, dErrorActivationMatrix, this.dOutputActivationFn)
+    }
 
-        // subtract the calculated derivatives
-        weights.sub(Matrix.mult(dErrorWeightMatrix, this.learningRate));
-        this.biases[layerIndex] = biases.map((value, index) => {
-            return value - dErrorBiasMatrix.get(index, 0)*this.learningRate;
-        });
+    /**
+     * Performs the back propagation algorithm
+     * @param {number[] | Matrix} targets The target outputs
+     */
+    _backPropagation(targets) {
 
-        // call recursively
-        this._backPropagation(layerIndex-1, dErrorPrevActivationMatrix);
+        let targetsMatrix = (targets instanceof Matrix) ? targets :
+                                                          Matrix.fromArray(targets, Matrix.COLUMN);
+        let outputLayer = this.layers[this.layers.length-1];
+
+        let dErrorActivationMatrix = this.dErrorFn(outputLayer.neurons, targetsMatrix)
+        for (let i = this.layers.length-1; i > 0; i++) {
+
+            let currLayer = this.layers[i];
+            let nextLayer = this.layers[i-1];
+
+            let {
+                dErrorWeightMatrix,
+                dErrorBiasMatrix,
+                dErrorPrevActivationMatrix
+            } = currLayer.gDFn(currLayer.neurons, nextLayer.neurons, currLayer.biases, currLayer.weights, 
+                               targetsMatrix, dErrorActivationMatrix, currLayer.dActivationFn);
+
+            // subtract the calculated derivatives
+            currLayer.weights.sub(Matrix.mult(dErrorWeightMatrix, this.learningRate));
+            currLayer.biases.sub(Matrix.mult(dErrorBiasMatrix, this.learningRate));
+
+            dErrorActivationMatrix = dErrorPrevActivationMatrix;  // to be used for next layer
+
+        }
 
     }
 
     train(inputs, targets) {
+
+        if (!this.layers) {
+            throw new Error("compile() must be called before training can take place.");
+        }
 
         this._feedForward(inputs, 0);
         this._backPropagation(this.neurons.length-1, null, targets);
@@ -194,8 +130,13 @@ class NeuralNetwork {
     }
 
     predict(inputs) {
-        this._feedForward(inputs, 0);
-        return this.neurons[this.neurons.length-1];
+
+        if (!this.layers) {
+            throw new Error("compile() must be called before predictions can be made.");
+        }
+        this._feedForward(inputs);
+        return this.layers[this.layers.length-1].neurons;
+        
     }
 
     toJSON() {
