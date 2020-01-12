7039f9ddf63adb37229c37abe83c0b550b8fc5bd
loldabigboi <loldabigboi@gmail.com>
2020-01-12 16:09:26 +1300

    Added PoolingLayer class

--- layer.js
+++ layer.js
@@ -58,7 +58,7 @@ class ConvolutionalLayer {
     constructor(options) {
 
         if (!options.inputDimensions) {
-            throw new Error("Input image dimensions not specified.");
+            throw new Error("Input dimensions not specified.");
         }
         this.inputDimensions = options.inputDimensions;
 
@@ -90,7 +90,66 @@ class ConvolutionalLayer {
 
 }
 
+class PoolingLayer {
+
+    constructor(options) {
+
+        if (!options.inputDimensions) {
+            throw new Error("Input dimensions not specified.");
+        }
+        this.inputDimensions = options.inputDimensions;
+
+        this.filterDimensions = options.filterDimensions ? options.filterDimensions : [3, 3];
+        for (let dimension of this.filterDimensions) {
+            if (dimension % 2 == 0 || dimension < 1) {
+                throw new Error("Filter dimension must be odd and > 0.");
+            }
+        }
+
+        this.type = options.type ? options.type : LayerConstants.MAX_POOLING;
+        if (this.type == LayerConstants.MAX_POOLING) {
+            this.poolingFn = function(arraySegment) {
+                // assumes arraySegment is a flattened array
+                let max = -Infinity;
+                for (let val of arraySegment) {
+                    if (val > max) {
+                        max = val;
+                    }
+                }
+                return max;
+            }
+        } else if (this.type == LayerConstants.MIN_POOLING) {
+            this.poolingFn = function(arraySegment) {
+                let min = Infinity;
+                for (let val of arraySegment) {
+                    if (val < min) {
+                        min = val;
+                    }
+                }
+                return min;
+            }
+        } else if (this.type == LayerConstants.AVG_POOLING){
+            this.poolingFn = function(arraySegment) {
+                let sum = 0;
+                for (let val of arraySegment) {
+                    sum += val;
+                }
+                return sum / arraySegment.length;
+            }
+        } else {
+            throw new Error("Invalid pooling type specified.");
+        }
+
+    }
+
+}
+
 class LayerConstants {}
+
+// pooling type constants
+LayerConstants.MAX_POOLING = "max-pooling";
+LayerConstants.MIN_POOLING = "max-pooling";
+LayerConstants.AVG_POOLING = "avg-pooling";
 
 // layer type constants
 LayerConstants.FULLY_CONNECTED = "fully-connected";
